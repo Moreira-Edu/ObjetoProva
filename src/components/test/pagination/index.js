fe68f78296a4b81c2e344e8d99fa444491bdb130
Luiz <luizctj@outlook.com>
2022-09-24 00:46:14 -0300

    feature: add new css refactor: svg

--- src/components/test/pagination/index.js
+++ src/components/test/pagination/index.js
@@ -51,6 +51,12 @@ function setCurrentPage(pageNumber) {
   swapClass(oldElement, newElement, "current-page");
 }
 
+export function renderButtonSVG(next) {
+  return next
+    ? "<svg xmlns='http://www.w3.org/2000/svg' width='1.5rem' height='1.5rem' fill='currentColor' viewBox='0 0 16 16'> <path fill-rule='evenodd'  d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z' /></svg>"
+    : "<svg xmlns='http://www.w3.org/2000/svg' width='1.5rem' height='1.5rem' fill='currentColor' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z' /></svg>";
+}
+
 export function renderPagination() {
   const pageSize = document.getElementById("questionsQuantity").value;
   const testTotal = Test.TestData.length;
@@ -83,7 +89,7 @@ export function renderPagination() {
       attributes: {
         class: "pagination-button",
       },
-      innerHTML: "<<",
+      innerHTML: renderButtonSVG(false),
       eventsListeners: {
         click: (e) => {
           e.preventDefault();
@@ -101,7 +107,7 @@ export function renderPagination() {
       attributes: {
         class: "pagination-button",
       },
-      innerHTML: ">>",
+      innerHTML: renderButtonSVG(true),
       eventsListeners: {
         click: (e) => {
           e.preventDefault();
